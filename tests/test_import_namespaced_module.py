f8907e530791fa9ddd8461542e43a929d6fc12b2
Steffen Hirte <shirte@users.noreply.github.com>
2025-05-01 13:10:47 +1200

    test: Test importing namespaced modules

---
+++ tests/test_import_namespaced_module.py
@@ -0,0 +1,9 @@
+import impynb
+
+
+def test_relative_import_of_notebook_as_module() -> None:
+    from .test_package.namespaced_module import module
+
+    assert module is not None
+    assert module.NOTEBOOK_WAS_IMPORTED is not None
+    assert module.NOTEBOOK_WAS_IMPORTED
