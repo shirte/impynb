efb98a1628d9383af72b9b0b73587618a40cdbe6
Steffen Hirte <shirte@users.noreply.github.com>
2025-05-01 13:04:49 +1200

    test: Check that exceptions in notebooks are displayed correctly

---
+++ tests/test_exceptions_from_notebooks.py
@@ -0,0 +1,21 @@
+import traceback
+
+import impynb
+
+
+def test_exceptions_from_notebooks() -> None:
+    try:
+        from .test_package import raises_an_exception
+    except Exception as e:
+        tb = e.__traceback__
+        extracted = traceback.extract_tb(tb)
+        last_frame = extracted[-1]
+
+        filename = last_frame.filename.rsplit("/", 1)[1]
+
+        assert filename == "raises_an_exception.ipynb[cell-1]"
+        assert last_frame.lineno == 2
+
+        # check that the code line is not empty (would be displayed as ???)
+        assert last_frame.line is not None
+        assert last_frame.line.strip() != ""
