9019bcbb2f798b861597b082dd961bb23ea06e4c
Steffen Hirte <shirte@users.noreply.github.com>
2025-05-02 12:34:14 +1200

    refactor: Adapt tests to context manager

--- tests/test_ignore_cells.py
+++ tests/test_ignore_cells.py
@@ -2,9 +2,8 @@ import impynb
 
 
 def test_ignoring_cells(clean_imports: None) -> None:
-    impynb.init()
-
-    from .test_package import contains_ignorable_cells
+    with impynb.configure(skip_cell_tags=[]):
+        from .test_package import contains_ignorable_cells
 
     assert contains_ignorable_cells is not None
     assert contains_ignorable_cells.NOTEBOOK_WAS_IMPORTED is not None
@@ -19,9 +18,8 @@ def test_ignoring_cells(clean_imports: None) -> None:
 
 def test_ignoring_cells_with_skip_cell_tags(clean_imports: None) -> None:
     # we configure impynb to skip cells with the tag "remove_cell"
-    impynb.init(skip_cell_tags=["remove_cell"])
-
-    from .test_package import contains_ignorable_cells
+    with impynb.configure(skip_cell_tags=["remove_cell"]):
+        from .test_package import contains_ignorable_cells
 
     # now, the cell with a tag should be ignored
     assert not hasattr(contains_ignorable_cells, "CELL_WITH_TAG_WAS_IGNORED")
