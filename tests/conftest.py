9dfc91f6d7d0b3649f0226900aed181a49c892e6
Steffen Hirte <shirte@users.noreply.github.com>
2025-05-02 13:22:46 +1200

    chore: Add missing type in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ import pytest
 
 
 @pytest.fixture
-def clean_imports():
+def clean_imports() -> None:
     to_remove = [k for k in sys.modules if k.startswith("tests.test_package")]
     for k in to_remove:
         del sys.modules[k]
