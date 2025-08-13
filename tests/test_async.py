5cbe6ac484dbd3b5e21b6b203efc7ec54d193a4a
Steffen Hirte <shirte@users.noreply.github.com>
2025-08-14 11:43:35 +1200

    test: Add test for custom event loops

--- tests/test_async.py
+++ tests/test_async.py
@@ -1,9 +1,20 @@
+import asyncio
+from unittest.mock import Mock
+
 import impynb
 
 
-def test_async() -> None:
+def test_async(clean_imports: None) -> None:
     from .test_package import async_cells
 
     assert async_cells is not None
     assert async_cells.NOTEBOOK_WAS_EXECUTED is not None
     assert async_cells.NOTEBOOK_WAS_EXECUTED
+
+
+def test_custom_event_loop(clean_imports: None) -> None:
+    mock_loop = Mock(spec=asyncio.AbstractEventLoop)
+    with impynb.configure(event_loop=mock_loop):
+        from .test_package import async_cells
+
+    mock_loop.run_until_complete.assert_called_once()
