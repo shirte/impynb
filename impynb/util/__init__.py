a8b98d9289f48627dd5ea4030d8ade79ac73baff
Steffen Hirte <shirte@users.noreply.github.com>
2025-08-17 14:47:19 +1200

    fix: Export util methods

--- impynb/util/__init__.py
+++ impynb/util/__init__.py
@@ -1,3 +1,5 @@
+from .get_notebook_path import get_notebook_path
 from .is_runnable_cell import is_runnable_cell
+from .is_running_in_notebook import is_running_in_notebook
 
-__all__ = ["is_runnable_cell"]
+__all__ = ["is_runnable_cell", "is_running_in_notebook", "get_notebook_path"]
