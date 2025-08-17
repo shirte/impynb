5a850e138614eb8ee293fd787ada6beb504447b7
Steffen Hirte <shirte@users.noreply.github.com>
2025-08-17 14:55:10 +1200

    chore: Clean cells and format code

--- tests/test_import_from_submodule.py
+++ tests/test_import_from_submodule.py
@@ -7,3 +7,11 @@ def test_import_from_submodule() -> None:
     assert is_in_a_submodule is not None
     assert is_in_a_submodule.NOTEBOOK_WAS_IMPORTED is not None
     assert is_in_a_submodule.NOTEBOOK_WAS_IMPORTED
+
+
+def test_relative_import_of_notebook_as_module() -> None:
+    from .test_package import import_some_local_module
+
+    assert import_some_local_module is not None
+    assert import_some_local_module.NOTEBOOK_WAS_IMPORTED_CORRECTLY is not None
+    assert import_some_local_module.NOTEBOOK_WAS_IMPORTED_CORRECTLY
