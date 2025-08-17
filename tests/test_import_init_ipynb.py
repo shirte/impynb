3cdea4552da69503eb2424033e2c40760e3e74f4
Steffen Hirte <shirte@users.noreply.github.com>
2025-08-17 14:50:01 +1200

    chore: Remove metadata from notebooks

--- tests/test_import_init_ipynb.py
+++ tests/test_import_init_ipynb.py
@@ -2,7 +2,10 @@ import impynb
 
 
 def test_import_init_ipynb() -> None:
-    from .test_package.submodule_with_init_ipynb import NOTEBOOK_WAS_IMPORTED, VARIABLE_WAS_SET
+    from .test_package.submodule_with_init_ipynb import (
+        NOTEBOOK_WAS_IMPORTED,
+        VARIABLE_WAS_SET,
+    )
 
     assert NOTEBOOK_WAS_IMPORTED is not None
     assert NOTEBOOK_WAS_IMPORTED
