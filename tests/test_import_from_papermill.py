4e7467e9ee47e463c22e50c732c2f0e4e8f710d6
Steffen Hirte <shirte@users.noreply.github.com>
2025-08-17 14:52:52 +1200

    test: Check import in papermill

---
+++ tests/test_import_from_papermill.py
@@ -0,0 +1,36 @@
+import os
+import tempfile
+
+import papermill as pm
+
+
+def test_import_from_notebook_env_vars() -> None:
+    with tempfile.TemporaryDirectory() as tmpdir:
+        relative_notebook_path = (
+            "./tests/test_package/import_from_papermill_env_vars.ipynb"
+        )
+
+        # set an environment variable to point to the notebook path
+        os.environ["NOTEBOOK_PATH"] = os.path.abspath(relative_notebook_path)
+
+        # Test that the notebook can be run with papermill
+        pm.execute_notebook(
+            relative_notebook_path,
+            os.path.join(tmpdir, "result_import_from_papermill_env_vars.ipynb"),
+            kernel_name="python3",
+        )
+
+
+def test_import_from_notebook_parameters() -> None:
+    with tempfile.TemporaryDirectory() as tmpdir:
+        relative_notebook_path = (
+            "./tests/test_package/import_from_papermill_parameters.ipynb"
+        )
+
+        # Test that the notebook can be run with papermill
+        pm.execute_notebook(
+            relative_notebook_path,
+            os.path.join(tmpdir, "result_import_from_papermill_parameters.ipynb"),
+            kernel_name="python3",
+            parameters={"NOTEBOOK_PATH": os.path.abspath(relative_notebook_path)},
+        )
