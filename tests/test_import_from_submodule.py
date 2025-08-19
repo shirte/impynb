51382d0affc69fa48c10a3e48187070b5a182230
Steffen Hirte <shirte@users.noreply.github.com>
2025-08-19 13:20:37 +1200

    test: Check importing local modules with relative path

--- tests/test_import_from_submodule.py
+++ tests/test_import_from_submodule.py
@@ -9,9 +9,17 @@ def test_import_from_submodule() -> None:
     assert is_in_a_submodule.NOTEBOOK_WAS_IMPORTED
 
 
-def test_relative_import_of_notebook_as_module() -> None:
+def test_relative_import_of_notebook_with_from() -> None:
     from .test_package import import_some_local_module
 
     assert import_some_local_module is not None
     assert import_some_local_module.NOTEBOOK_WAS_IMPORTED_CORRECTLY is not None
     assert import_some_local_module.NOTEBOOK_WAS_IMPORTED_CORRECTLY
+
+
+def test_relative_import_of_notebook() -> None:
+    from .test_package import import_notebook_relative_path
+
+    assert import_notebook_relative_path is not None
+    assert import_notebook_relative_path.NOTEBOOK_WAS_IMPORTED is not None
+    assert import_notebook_relative_path.NOTEBOOK_WAS_IMPORTED
---
+++ tests/test_package/import_notebook_relative_path.py
@@ -0,0 +1,5 @@
+from . import some_notebook_to_import
+
+assert some_notebook_to_import is not None
+
+NOTEBOOK_WAS_IMPORTED = some_notebook_to_import.NOTEBOOK_WAS_IMPORTED
